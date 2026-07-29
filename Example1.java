e4bff94632262640b9c33bf5d3e030d438a5e149
Sarthak <sarthakshete090909@gmail.com>
2026-07-30 00:08:57 +0530

    Initial Commit

---
+++ Example1.java
@@ -0,0 +1,16 @@
+package Raj;
+public class Example1
+{
+	private int x;
+	private int y;
+	public void setData(int p, int q)
+	{
+		x=p;
+		y=q;
+	}
+	public void displayData()
+	{
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
---
+++ Example2.java
@@ -0,0 +1,12 @@
+package Simran;
+import Raj.Example1;
+
+class Example2
+{
+	public static void main(String args[])
+	{
+		Example1 e1=new Example1();
+		e1.setData(45,18);
+		e1.displayData();
+	}
+}
